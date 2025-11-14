cc7f997f86d076af95a22237e417061ef94a2114
Guilherm_Costa <guidacosta130207@gmail.com>
2025-11-13 21:00:51 -0300

    TSK-02 concluída.

--- src/client.c
+++ src/client.c
@@ -1,3 +1,10 @@
-//
-// Created by murandido on 11/4/25.
-//
+#include "../include/client.h"
+
+typedef  struct {
+    int id;
+    char cpf[12];
+    char name[100];
+    char  fullAddress[100];
+    char phoneNumber[20];
+    char email[100];
+} Individual;
