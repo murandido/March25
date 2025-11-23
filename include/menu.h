60e625b2bcc75b05f9836c6afd275b9c54d71cd3
Murandido <murilo.candidomc97@gmail.com>
2025-11-23 13:09:50 -0300

    feat: add create windows logic

--- include/menu.h
+++ include/menu.h
@@ -1,7 +1,6 @@
-#include <ncursesw/curses.h>
-
 #ifndef MARCH25_MENU_H
 #define MARCH25_MENU_H
+#include <ncursesw/curses.h>
 
 void drawBorderWindow(WINDOW *borderWindow, int mainBlockW, int menuW, int menuSuppW, int topRowH);
 
