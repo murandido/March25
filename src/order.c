a8dc7cd042646884243703b51725bfac3cc1a8bd
Murandido <murilo.candidomc97@gmail.com>
2025-11-24 14:01:15 -0300

    feat: create initOrderList() function

--- src/order.c
+++ src/order.c
@@ -1,1 +1,10 @@
+#include <stdlib.h>
 #include "../include/order.h"
+
+#define INITIAL_CAPACITY 10
+
+void initOrderList(OrderList *list) {
+    list->data = (Order*) malloc(INITIAL_CAPACITY * sizeof(Order));
+    list->count = 0;
+    list->capacity = INITIAL_CAPACITY;
+}
