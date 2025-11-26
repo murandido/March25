e28f92f100d61abb436c4db44e0739db54b9c6c9
Guilherm_Costa <guidacosta130207@gmail.com>
2025-11-25 23:11:37 -0300

    feat: create ClientList struct

--- include/client.h
+++ include/client.h
@@ -8,11 +8,17 @@ typedef struct {
     char legalName[100];
     char address[200];
     char phoneNumber[20];
-    char cpf[12];
+    char cpf[15];
     char cnpj[15];
     char email[100];
     char contactName[100];
 } Client;
+
+typedef struct {
+    Client *data;
+    int count;
+    int capacity;
+} ClientList;
 
 int validateCNPJ(const char *cnpjInput);
 int sameNumbers(const int *cpf_nums);
